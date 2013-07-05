bf489a9bcf41827f9aaa737c3e7579e8d3e6dab1
tknhs <kdsnlv0109@gmail.com>
2013-07-05 23:28:13 +0900

    所蔵していない場合も視覚化

--- myscript.js
+++ myscript.js
@@ -13,15 +13,16 @@ xhr.onreadystatechange = function() {
     var book = linkit.getElementsByTagName("a")[1];
 
     // LC have this Book.
+    var ele = document.createElement("p");
     if (book != null) {
-      console.log('ok');
-      var ele = document.createElement("p");
+      //console.log('ok');
       ele.innerHTML = "<a href="+lc+" target='_blank'><font size='5' color='#006400'>LCに所蔵されています</font></a>";
-      document.getElementById("actualPriceExtraMessaging").appendChild(ele);
     }
     else {
-      console.log('NG');
+      //console.log('NG');
+      ele.innerHTML = "<font size='5' color='#990012'>LCに所蔵されていません</font>";
     }
+    document.getElementById("actualPriceExtraMessaging").appendChild(ele);
   }
 }
 xhr.send();
