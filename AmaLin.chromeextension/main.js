3f7a25b13c6bf07528a8777d374c25d70056e837
tknhs <kdsnlv0109@gmail.com>
2013-07-07 01:17:06 +0900

    本のときだけ表示するように修正

--- AmaLin.chromeextension/main.js
+++ AmaLin.chromeextension/main.js
@@ -1,29 +1,35 @@
 var url = location.pathname;
-var u_isbn = url.match(/\/(dp|gp)(\/product)?\/([0-9a-zA-Z]+)/).toString();
-var isbn = RegExp.$3;
 
-var lc = "http://linkit.kanazawa-it.ac.jp/opac/cgi/searchS.cgi?AC=1&RI10=IB&SW10="+isbn
-var xhr = new XMLHttpRequest();
-xhr.open("GET", lc, true);
-xhr.onreadystatechange = function() {
+/* Only the books */
+try {
+  var u_isbn = url.match(/\/(dp|gp)(\/product)?\/([0-9]+)(X)?/).toString();
+  var isbn = RegExp.$3 + RegExp.$4;
 
-  if (xhr.readyState == 4 && xhr.status==200) {
-    var linkit = document.createElement("linkit");
-    linkit.innerHTML = xhr.responseText;
-    var book = linkit.getElementsByTagName("a")[1];
+  var lc = "http://linkit.kanazawa-it.ac.jp/opac/cgi/searchS.cgi?AC=1&SC=F&RI10=IB&SW10="+isbn
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", lc, true);
+  xhr.onreadystatechange = function() {
 
-    // LC have this Book.
-    var ele = document.createElement("p");
-    if (book != null) {
-      //console.log('ok');
-      ele.innerHTML = "<a href="+lc+" target='_blank'><font size='5' color='#006400'>LCに所蔵されています</font></a>";
+    if (xhr.readyState == 4 && xhr.status==200) {
+      var linkit = document.createElement("linkit");
+      linkit.innerHTML = xhr.responseText;
+      var book = linkit.getElementsByTagName("a")[1];
+
+      // LC have this Book.
+      var ele = document.createElement("p");
+      if (book != null) {
+        //console.log('ok');
+        ele.innerHTML = "<a href="+lc+" target='_blank'><font size='5' color='#006400'>LCに所蔵されています</font></a>";
+      }
+      else {
+        //console.log('NG');
+        ele.innerHTML = "<font size='5' color='#990012'>LCに所蔵されていません</font>";
+      }
+      document.getElementById("actualPriceExtraMessaging").appendChild(ele);
     }
-    else {
-      //console.log('NG');
-      ele.innerHTML = "<font size='5' color='#990012'>LCに所蔵されていません</font>";
-    }
-    document.getElementById("actualPriceExtraMessaging").appendChild(ele);
   }
+  xhr.send();
 }
-xhr.send();
-
+catch(e) {
+  /* Except the books */
+}
--- AmaLin.safariextension/main.js
+++ AmaLin.safariextension/main.js
@@ -1,24 +1,31 @@
 var url = location.pathname;
-var u_isbn = url.match(/\/(dp|gp)(\/product)?\/([0-9a-zA-Z]+)/).toString();
-var isbn = RegExp.$3;
-var lc = "http://linkit.kanazawa-it.ac.jp/opac/cgi/searchS.cgi?AC=1&RI10=IB&SW10="+isbn
 
-/* send to globalpage */
-safari.self.tab.dispatchMessage("URL", lc);
+/* Only the books */
+try {
+  var u_isbn = url.match(/\/(dp|gp)(\/product)?\/([0-9]+)(X)?/).toString();
+  var isbn = RegExp.$3 + RegExp.$4;
+  var lc = "http://linkit.kanazawa-it.ac.jp/opac/cgi/searchS.cgi?AC=1&SC=F&RI10=IB&SW10="+isbn
 
-/* get from globalpage */
-safari.self.addEventListener("message", function(evt){
-  var bool = evt.message;
-  //console.log(bool);
+  /* send to globalpage */
+  safari.self.tab.dispatchMessage("URL", lc);
 
-  /* LC have this Book. */
-  var ele = document.createElement("p");
-  if (bool == true) {
-    ele.innerHTML = "<a href="+lc+" target='_blank'><font size='5' color='#006400'>LCに所蔵されています</font></a>";
-  }
-  else {
-    ele.innerHTML = "<font size='5' color='#990012'>LCに所蔵されていません</font>";
-  }
-  document.getElementById("actualPriceExtraMessaging").appendChild(ele);
-}, false);
+  /* get from globalpage */
+  safari.self.addEventListener("message", function(evt){
+    var bool = evt.message;
+    //console.log(bool);
+
+    /* LC have this Book. */
+    var ele = document.createElement("p");
+    if (bool == true) {
+      ele.innerHTML = "<a href="+lc+" target='_blank'><font size='5' color='#006400'>LCに所蔵されています</font></a>";
+    }
+    else {
+      ele.innerHTML = "<font size='5' color='#990012'>LCに所蔵されていません</font>";
+    }
+    document.getElementById("actualPriceExtraMessaging").appendChild(ele);
+  }, false);
+}
+catch(e) {
+  /* Except the books */
+}
 
