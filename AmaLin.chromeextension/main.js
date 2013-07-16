899d418f43058699352fb6da0aedd3fb3d5eb9d2
tknhs <kdsnlv0109@gmail.com>
2013-07-16 14:49:33 +0900

    出品者から〜のときに表示されない不具合を修正

--- AmaLin.chromeextension/main.js
+++ AmaLin.chromeextension/main.js
@@ -5,7 +5,7 @@ try {
   var u_isbn = url.match(/\/(dp|gp)(\/product)?\/([0-9]+)(X)?/).toString();
   var isbn = RegExp.$3 + RegExp.$4;
 
-  var lc = "http://linkit.kanazawa-it.ac.jp/opac/cgi/searchS.cgi?AC=1&SC=F&RI10=IB&SW10="+isbn
+  var lc = "http://linkit.kanazawa-it.ac.jp/opac/cgi/searchS.cgi?AC=1&SC=F&RI10=IB&SW10="+isbn;
   var xhr = new XMLHttpRequest();
   xhr.open("GET", lc, true);
   xhr.onreadystatechange = function() {
@@ -18,20 +18,31 @@ try {
       // LC have this Book.
       var ele = document.createElement("p");
       if (book != null) {
-        //console.log('ok');
+        console.log("have? : yes");
         ele.innerHTML = "<a href="+lc+" target='_blank'><font size='5' color='#006400'>LCに所蔵されています</font></a>";
       }
       else {
-        //console.log('NG');
+        console.log("have? : no");
         ele.innerHTML = "<font size='5' color='#990012'>LCに所蔵されていません</font>";
       }
-      document.getElementById("actualPriceExtraMessaging").appendChild(ele);
+
+      // insert place
+      var i = 0;
+      var nodelist = document.getElementsByClassName("buying");
+      for (var nl = 0; nl < nodelist.length; nl++){
+        if (nodelist[nl].id == ""){
+          i++;
+          if (i == 2){
+            nodelist[nl].parentNode.insertBefore(ele, nodelist[nl]);
+          }
+        }
+      }
     }
   }
   xhr.send();
 }
 catch(e) {
   /* Except the books */
-  //console.log(e.name);
-  //console.log(e.message);
+  console.log(e.name);
+  console.log(e.message);
 }
--- AmaLin.safariextension/main.js
+++ AmaLin.safariextension/main.js
@@ -17,17 +17,30 @@ try {
     /* LC have this Book. */
     var ele = document.createElement("p");
     if (bool == true) {
+      console.log("have? : yes");
       ele.innerHTML = "<a href="+lc+" target='_blank'><font size='5' color='#006400'>LCに所蔵されています</font></a>";
     }
     else {
+      console.log("have? : no");
       ele.innerHTML = "<font size='5' color='#990012'>LCに所蔵されていません</font>";
     }
-    document.getElementById("actualPriceExtraMessaging").appendChild(ele);
+
+    // insert place
+    var i = 0;
+    var nodelist = document.getElementsByClassName("buying");
+    for (var nl = 0; nl < nodelist.length; nl++){
+      if (nodelist[nl].id == ""){
+        i++;
+        if (i == 2){
+          nodelist[nl].parentNode.insertBefore(ele, nodelist[nl]);
+        }
+      }
+    }
   }, false);
 }
 catch(e) {
   /* Except the books */
-  //console.log(e.name);
-  //console.log(e.message);
+  console.log(e.name);
+  console.log(e.message);
 }
 
